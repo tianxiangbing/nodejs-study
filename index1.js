b358b049b311e7b47e859704568a081fba6a7931
tianxiangbing <55342775@qq.com>
2017-02-27 09:24:42 +0800

    s

--- index1.js
+++ index1.js
@@ -1,12 +1,31 @@
-
-setImmediate(function(){
+/// <reference path="http" />
+setImmediate(function () {
 	console.log(4);
 })
 console.log(1);
-process.nextTick(function(){
+process.nextTick(function () {
 	console.log(3);
 });
 console.log(2);
-process.nextTick(function(){
+process.nextTick(function () {
 	console.log(5)
+});
+var EventEmitter = require('events').EventEmitter;
+// var EventEmitter = process.EventEmitter,
+	MyClass = function () { };
+MyClass.prototype.__proto__ = EventEmitter.prototype;
+var a  = new MyClass();
+a.on('dosome',function(){
+	console.log('do some things.');
+})
+a.emit('dosome');
+var http = require('http');
+http.createServer(function(req,res){
+	var buf = '';
+	req.on('data',function(data){
+		buf +=data;
+	});
+	req.on('end',function(){
+		console.log('数据接收完毕');
+	});
 })
