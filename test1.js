cd6bf8cae2c0a725e584dc0128cc57643ea03b2d
tianxiangbing <55342775@qq.com>
2017-03-05 19:39:10 +0800

    x

---
+++ test1.js
@@ -0,0 +1,38 @@
+function Animal(){
+    this.name = 'Animal';
+    console.log('Animal init..');
+}
+function Dog(){
+    this.name='Dog';
+}
+Animal.prototype.say=function(){
+    console.log(this.name)
+}
+function Cat (){
+    this.name='Cat';
+}
+Cat.prototype.__proto__=Animal.prototype;
+var cat = new Cat()
+cat.say();
+
+Dog.prototype= new Animal;
+var dog = new Dog()
+dog.say();
+
+Date.prototype.__defineGetter__('ago',function(){
+    var diff = ((new Date()).getTime() - this.getTime())/1000;
+    var day_diff = Math.floor(diff/(24*60*60));
+    console.log(this)
+    return diff;
+});
+
+var a = new Date('2/18/2017');
+console.log(a.ago);
+var now = new Date();
+setTimeout(function(){
+    console.log(Date.now()-now);
+    for(var i=0 ;i <1000000000;i++);
+},1000);
+setTimeout(function(){
+    console.log(Date.now()-now);
+},2000);
